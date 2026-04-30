a7b23ceabc43e3f27b56907361384651ea6649d4
bhupinder <bhupinder@gmail.com>
2026-04-30 22:54:41 +0530

    db_setup

--- src/main/java/com/app/ecom/User.java
+++ src/main/java/com/app/ecom/User.java
@@ -1,13 +1,22 @@
 package com.app.ecom;
 
+import jakarta.validation.constraints.Pattern;
+import lombok.Generated;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
 import lombok.Data;
+import jakarta.validation.constraints.NotBlank;
 
 @Data
+@Document(collection = "users")
 public class User {
-    private Long id;
+    @Id
+
+    private String id;
+    private Long serialId;
+    @NotBlank(message = "First name is required")
+    @Pattern(regexp = "^[a-zA-Z]+$", message = "First name must contain only letters")
     private String firstName;
+    @NotBlank(message = "Last name is required")
     private String lastName;
-
-
-
 }
---
+++ src/main/java/com/app/ecom/UserService.java
@@ -0,0 +1,29 @@
+package com.app.ecom;
+
+import org.springframework.stereotype.Service;
+import lombok.RequiredArgsConstructor;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+    private final UserRepository userRepository;
+    private Long nextId = 101L;
+
+    public List<User> fetchAllUsers() {
+        return userRepository.findAll();
+    }
+
+    public void addUser(User user) {
+        user.setSerialId(nextId++);
+        userRepository.save(user);
+    }
+
+    // UserService.java
+    public User fetchUsers(Long serialId) {
+        return userRepository.findBySerialId(serialId);
+    }
+
+
+
+}

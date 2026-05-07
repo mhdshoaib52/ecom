eaa97fb6c1897cb600c8cb3bd41ca11bec985dde
bhupinder <bhupinder@gmail.com>
2026-05-07 15:52:19 +0530

    Productclasses

---
+++ src/main/java/com/app/ecom/repository/ProductRepository.java
@@ -0,0 +1,10 @@
+package com.app.ecom.repository;
+
+import com.app.ecom.model.Product;
+import com.app.ecom.model.User;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepository extends MongoRepository<Product, Long> {
+}

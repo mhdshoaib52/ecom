3f0f4b364599c7b9606708f2758420054e782989
bhupinder <bhupinder@gmail.com>
2026-05-14 18:38:38 +0530

    cart controller

--- src/main/java/com/app/ecom/controller/CartController.java
+++ src/main/java/com/app/ecom/controller/CartController.java
@@ -1,18 +1,25 @@
 package com.app.ecom.controller;
 
 
+import com.app.ecom.service.CartService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("/api/cart")
+
 public class CartController {
+    private final CartService cartService;
     @PutMapping
     public ResponseEntity<void> addToCart(
-            @RequestHeader("X-User_ID") String userId;
-            CartItemRequest request
-    )
+            @RequestHeader("X-User_ID") String userId,
+             @RequestBody CartItemRequest request){
+
+        cartService.addToCart(userId,request);
+        return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+
 }
